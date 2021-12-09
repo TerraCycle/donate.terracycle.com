b262c2013d7aa4d8fba40da1d2ba611ce9143b43
gergo.toth <gergo.toth@bigfish.hu>
2021-12-09 11:02:16 +0100

    Missing logos

--- src/en-CA/blenheimkentgoldeneaglesgymnasticsclub/blenheimkentgoldeneaglesgymnasticsclub.11tydata.js
+++ src/en-CA/blenheimkentgoldeneaglesgymnasticsclub/blenheimkentgoldeneaglesgymnasticsclub.11tydata.js
@@ -54,7 +54,11 @@ module.exports = () => {
                 paragraphs: [`Send ${site.brandName}® your trash (free&nbsp;shipping) to be recycled; ${site.brandName} will donate $10 to ${charityName}`],
             },
             introduction: {
-                charityLogoFileName: "", // ⚠️ Missing logo!
+                charityLogoFileName: "blenheim_kent_golden_eagles_gymnastics_club.jpg",
+                charityLogoHeight: {
+                    mobile: 70,
+                    desktop: 130,
+                },
                 paragraphs: [
                     `This holiday season, you can donate to <strong>${charityName}</strong> by recycling through ${site.brandName} at no cost to you! By sending your trash to ${site.brandName} (free&nbsp;shipping), you’ll make a positive impact on the environment and support <strong>${charityName}</strong>. Send your used, hard-to-recycle products and packaging to TerraCycle, and they’ll donate $10 per shipment received to <strong>${charityName}</strong>.`,
                     `${site.brandName} is opening ${charityPrograms.length} recycling programs for participation through the end of the year.`,
--- src/en-US/eisenhowerelementaryschool/eisenhowerelementaryschool.11tydata.js
+++ src/en-US/eisenhowerelementaryschool/eisenhowerelementaryschool.11tydata.js
@@ -126,7 +126,11 @@ module.exports = () => {
                 paragraphs: [`Send ${site.brandName}® your trash (free&nbsp;shipping) to be recycled;`, `${site.brandName} will donate $10 to <strong>${charityName}</strong>`],
             },
             introduction: {
-                charityLogoFileName: "", // ⚠️ Missing logo!
+                charityLogoFileName: "eisenhower_elementary_school.jpg",
+                charityLogoHeight: {
+                    mobile: 60,
+                    desktop: 110,
+                },
                 paragraphs: [
                     `This holiday season, you can donate to <strong>${charityName}</strong> by recycling through ${site.brandName} <em>at no cost to you!</em> By sending your trash to ${site.brandName} (free&nbsp;shipping), you’ll make a positive impact on the environment and support <strong>${charityName}</strong>. Send your used, hard-to-recycle products and packaging to ${site.brandName}, and they’ll donate $10 per shipment received to <strong>${charityName}</strong>.`,
                     `${site.brandName} is opening ${charityPrograms.length} recycling programs for participation through the end of the year.`,
